376aef312edc2ba48ca724a03e0ba82a0970d8c1
AndreiChornii <andrei.chornii@gmail.com>
2020-02-03 15:45:17 +0200

    travelcream_js_2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 `use strict`;
 
-//let form = document.querySelector('#form_wrapper form');
-////console.log(form);
-//let buttom_send = form[3];
-//console.log(buttom_send);
-//buttom_send.prev
-
 function sendedData(form){
-//    let form = document.querySelector('#form_wrapper form');
-//    console.log(form);
+
     let from = form[0].value;
     let to = form[1].value;
     let date = form[2].value;
